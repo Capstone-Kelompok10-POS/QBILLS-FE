cc16c5393ae795fb90457091b95bcf03c3717836
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-07 16:39:39 +0700

    module - Example Page

---
+++ src/modules/example-page/header/navbar/index.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <p>Navbar</p>
+    </nav>
+  );
+};
---
+++ src/modules/example-page/main/index.jsx
@@ -0,0 +1,9 @@
+import { About } from "./about";
+
+export const Main = () => {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+};
