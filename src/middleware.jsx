0145b45781e83574b425bba81aac0dcfbea1f54e
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-14 06:14:26 +0700

    feat - Role Based Access Control

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,2 +1,20 @@
-export { default } from "next-auth/middleware";
-export const config = { matcher: ["/dashboard"] };
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(request) {
+    if (
+      request.nextUrl.pathname.startsWith("/superadmin") &&
+      request.nextauth.token?.results.role !== "superadmin"
+    ) {
+      return NextResponse.rewrite(new URL("/login", request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  },
+);
+
+export const config = { matcher: ["/dashboard", "/superadmin"] };
