333832c7b423d626d971434d8806b2e259e1831c
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-14 18:47:38 +0700

    update - Chip | Component

--- src/components/chip/index.jsx
+++ src/components/chip/index.jsx
@@ -7,15 +7,31 @@ export const Chip = ({ size, startIcon, label, endIcon, onClick, selected, disab
       onClick={onClick}
       disabled={disabled}
       className={clsx("flex h-fit w-fit items-center justify-center rounded-full border", {
-        // STYLE
+        // VARIANT
         "border-N2 text-N7 hover:border-P5 hover:bg-P5 hover:text-N1 active:scale-95 active:border-P6 active:bg-P6 active:text-N1":
           !selected && !disabled,
+
+        // SELECTED
         "cursor-default border-P6 bg-P6 text-N1": selected && !disabled,
-        "cursor-not-allowed border-N3 bg-N2 text-N3": disabled && !selected,
+
+        // DISABLED
+        "cursor-not-allowed border-N3 bg-N2 text-N3": disabled,
 
         // SIZE
-        "gap-1 px-4 py-2": size === "md",
+        "gap-1 px-3 py-2": size === "sm",
+        "gap-1 px-4 py-2.5": size === "md",
         "gap-2 px-5 py-3": size === "lg",
+        "gap-2 px-6 py-3.5": size === "xl",
+        "gap-2 px-7 py-4": size === "2xl",
+        "gap-2 px-8 py-5": size === "3xl",
+
+        // FONT SIZE
+        "text-sm": size === "sm",
+        "text-base": size === "md",
+        "text-lg": size === "lg",
+        "text-xl": size === "xl",
+        "text-2xl": size === "2xl",
+        "text-3xl": size === "3xl",
       })}
     >
       {startIcon}
