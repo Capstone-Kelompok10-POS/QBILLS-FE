cf9f813f2be4fd8bc8d970653f860b6eab700e0a
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-15 12:53:23 +0700

    update - Icon Button | Component

--- src/components/icon-button/index.jsx
+++ src/components/icon-button/index.jsx
@@ -1,23 +1,29 @@
 import clsx from "clsx";
 
-export const IconButton = ({ size, shadow, icon, onClick, disabled }) => {
+export const IconButton = ({ variant, size, icon, onClick, disabled }) => {
   return (
     <button
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className={clsx("flex items-center justify-center rounded-lg", {
-        // STYLE
-        "bg-P4 text-N1 hover:bg-P5 active:scale-95 active:bg-P6": !disabled,
-        "cursor-not-allowed bg-N2 text-N3": disabled,
+      className={clsx("flex items-center justify-center rounded-xl border", {
+        // VARIANT
+        "border-P4 bg-P4 text-N1 hover:border-P5 hover:bg-P5 active:scale-95 active:border-P6 active:bg-P6":
+          !variant && !disabled,
+        "border-P4 text-P4 hover:border-P5 hover:bg-P5 hover:text-N1 active:scale-95 active:border-P6 active:bg-P6 active:text-N1":
+          variant === "outline" && !disabled,
 
-        // SHADOW
-        "shadow-md shadow-N7/40": shadow,
+        // DISABLED
+        "cursor-not-allowed bg-N2 text-N3": !variant && disabled,
+        "cursor-not-allowed border-N3 text-N3": variant === "outline" && disabled,
 
         // SIZE
-        "h-[40px] w-[40px]": size === "sm",
-        "h-[48px] w-[48px]": size === "md",
-        "h-[56px] w-[56px]": size === "lg",
+        "p-1.5": size === "sm",
+        "p-2": size === "md",
+        "p-2.5": size === "lg",
+        "p-3": size === "xl",
+        "p-3.5": size === "2xl",
+        "p-4": size === "3xl",
       })}
     >
       {icon}
