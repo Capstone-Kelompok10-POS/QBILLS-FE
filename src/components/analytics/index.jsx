949e6698dbd06313531aeda5cec94abdf00a23d1
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-18 04:33:02 +0700

    update - Analytics | Component

--- src/components/analytics/index.jsx
+++ src/components/analytics/index.jsx
@@ -1,13 +1,11 @@
 export const Analytics = ({ icon, label, numberData }) => {
   return (
-    <div className="flex w-full flex-col items-start justify-center gap-4 rounded-lg border border-N2 p-8 shadow-sm">
-      <div className="flex-shrink-0">
-        {icon && <div className="h-12 w-12 overflow-hidden">{icon}</div>}
-      </div>
+    <section className="flex w-full flex-col items-start justify-center gap-4 rounded-lg border border-N2 p-8">
+      {icon}
       <div className="whitespace-nowrap text-base font-semibold leading-5 text-N3">{label}</div>
       <div className="whitespace-nowrap text-3xl font-semibold leading-10 text-N7">
         {numberData}
       </div>
-    </div>
+    </section>
   );
 };
