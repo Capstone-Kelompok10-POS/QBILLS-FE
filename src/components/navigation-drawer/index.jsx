40c9a66c1c464de855061c1d3e3467eda994953b
helastro <helmyfirdy888@gmail.com>
2023-11-17 10:10:27 +0700

    update - Navigation Drawer | Component

--- src/components/navigation-drawer/index.jsx
+++ src/components/navigation-drawer/index.jsx
@@ -2,20 +2,30 @@ import logoQbills from "@/public/assets/images/logos/brown/logo-2.png";
 import logoQbills2 from "@/public/assets/images/logos/brown/logo-4.png";
 import logoQbills3 from "@/public/assets/images/logos/brown/logo-5.png";
 import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import DashboardIcon from "@mui/icons-material/Dashboard";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import AddBoxIcon from "@mui/icons-material/AddBox";
 import LocalCafeOutlinedIcon from "@mui/icons-material/LocalCafeOutlined";
+import LocalCafeIcon from "@mui/icons-material/LocalCafe";
 import LocalPoliceOutlinedIcon from "@mui/icons-material/LocalPoliceOutlined";
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
 import PaymentOutlinedIcon from "@mui/icons-material/PaymentOutlined";
+import PaymentIcon from "@mui/icons-material/Payment";
 import LeaderboardOutlinedIcon from "@mui/icons-material/LeaderboardOutlined";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import Image from "next/image";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 export const NavigationDrawer = () => {
+  const pathname = usePathname();
   return (
-    <nav className="bg-N1.1 fixed left-0 top-0 flex h-screen w-96 flex-col gap-14 px-6 py-14 text-N3">
+    <nav className="fixed left-0 top-0 flex h-screen w-96 flex-col gap-14 bg-N1.1 px-6 py-14 text-N3">
       <section className="flex w-full items-center justify-center gap-2">
         <Image src={logoQbills} alt="Qbills" width={60} quality={30} priority className="h-auto" />
         <div className="flex flex-col items-center justify-center">
@@ -40,45 +50,84 @@ export const NavigationDrawer = () => {
 
       <section>
         <ul className="flex w-full flex-col gap-4 text-xl font-semibold">
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/dashboard",
+              "bg-P2 text-P6": pathname === "/dashboard",
+            })}
+          >
             <Link href={"/dashboard"} className="flex items-center gap-4">
-              <DashboardOutlinedIcon />
+              {pathname === "/dashboard" ? <DashboardIcon /> : <DashboardOutlinedIcon />}
               Dashboard
             </Link>
           </li>
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/manageaccount",
+              "bg-P2 text-P6": pathname === "/manageaccount",
+            })}
+          >
             <Link href={"/manageaccount"} className="flex items-center gap-4">
-              <AccountCircleOutlinedIcon />
+              {pathname === "/manageaccount" ? (
+                <AccountCircleIcon />
+              ) : (
+                <AccountCircleOutlinedIcon />
+              )}
               Manage Account
             </Link>
           </li>
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/manageproduct",
+              "bg-P2 text-P6": pathname === "/manageproduct",
+            })}
+          >
             <Link href={"/manageproduct"} className="flex items-center gap-4">
-              <AddBoxOutlinedIcon />
+              {pathname === "/manageproduct" ? <AddBoxIcon /> : <AddBoxOutlinedIcon />}
               Manage Product
             </Link>
           </li>
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/listproduct",
+              "bg-P2 text-P6": pathname === "/listproduct",
+            })}
+          >
             <Link href={"/listproduct"} className="flex items-center gap-4">
-              <LocalCafeOutlinedIcon />
+              {pathname === "/listproduct" ? <LocalCafeIcon /> : <LocalCafeOutlinedIcon />}
               List Product
             </Link>
           </li>
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/membership",
+              "bg-P2 text-P6": pathname === "/membership",
+            })}
+          >
             <Link href={"/membership"} className="flex items-center gap-4">
-              <LocalPoliceOutlinedIcon />
+              {pathname === "/membership" ? <LocalPoliceIcon /> : <LocalPoliceOutlinedIcon />}
               Membership
             </Link>
           </li>
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/transaction",
+              "bg-P2 text-P6": pathname === "/transaction",
+            })}
+          >
             <Link href={"/transaction"} className="flex items-center gap-4">
-              <PaymentOutlinedIcon />
+              {pathname === "/transaction" ? <PaymentIcon /> : <PaymentOutlinedIcon />}
               Transaction
             </Link>
           </li>
-          <li className="w-full cursor-pointer rounded-lg p-4 hover:bg-P2 hover:text-P6 active:scale-95">
+          <li
+            className={clsx("w-full cursor-pointer rounded-lg p-4", {
+              "hover:bg-P2 hover:text-P6 active:scale-95": pathname !== "/report",
+              "bg-P2 text-P6": pathname === "/report",
+            })}
+          >
             <Link href={"/report"} className="flex items-center gap-4">
-              <LeaderboardOutlinedIcon />
+              {pathname === "/report" ? <LeaderboardIcon /> : <LeaderboardOutlinedIcon />}
               Report
             </Link>
           </li>
