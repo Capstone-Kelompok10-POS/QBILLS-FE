99553b8bab704cd329f507984c42cde15eb93421
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-18 04:37:28 +0700

    update - Analytics | Module

--- src/modules/components/analytics/index.jsx
+++ src/modules/components/analytics/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import { Analytics } from "@/components";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import ImportExportIcon from "@mui/icons-material/ImportExport";
 import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
-import { Analytics } from "@/components";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
 export const ComponentAnalytics = () => {
   return (
     <>
+      {/*
+          icon={</>}
+          label={""}
+          numberData={""}
+      */}
+
       <section className="flex w-full">
         <div className="flex w-full gap-5">
           <Analytics
@@ -15,16 +20,19 @@ export const ComponentAnalytics = () => {
             label={"Daily Total"}
             numberData={"Rp.560.000"}
           />
+
           <Analytics
             icon={<ImportExportIcon sx={{ fontSize: 50 }} className="text-S4" />}
             label={"Success"}
             numberData={"31"}
           />
+
           <Analytics
             icon={<QueryBuilderIcon sx={{ fontSize: 50 }} className="text-W4" />}
             label={"Pending"}
             numberData={"4"}
           />
+
           <Analytics
             icon={<RemoveCircleOutlineIcon sx={{ fontSize: 50 }} className="text-E4" />}
             label={"Canceled"}
