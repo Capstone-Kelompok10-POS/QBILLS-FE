03d02dd084697bc6a32e0e8a0f342d0eab4fca79
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-18 04:33:12 +0700

    update - Chip | Component

--- src/components/chip/index.jsx
+++ src/components/chip/index.jsx
@@ -1,37 +1,40 @@
 import clsx from "clsx";
 
-export const Chip = ({ size, startIcon, label, endIcon, onClick, selected, disabled, variant }) => {
+export const Chip = ({ variant, size, startIcon, label, endIcon, onClick, selected, disabled }) => {
   return (
     <button
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className={clsx("flex h-fit w-fit items-center justify-center rounded-full border", {
+      className={clsx("flex items-center justify-center rounded-full border", {
         // NORMAL
         "border-N2  hover:border-P5 hover:bg-P5 hover:text-N1 active:scale-95 active:border-P6 active:bg-P6 active:text-N1":
-          !selected && !disabled,
+          !variant && !selected && !disabled,
 
         // VARIANT
-        "bg-S1 text-S5": variant === "success",
-        "bg-W1 text-W5": variant === "pending",
-        "bg-E1 text-E5": variant === "canceled",
+        "cursor-default border-S1 bg-S1 text-S5": variant === "success",
+        "cursor-default border-W1 bg-W1 text-W5": variant === "pending",
+        "cursor-default border-E1 bg-E1 text-E5": variant === "canceled",
 
         // SELECTED
-        "cursor-default border-P6 bg-P6 text-N1": selected && !disabled,
+        "cursor-default border-P6 bg-P6 text-N1": !variant && selected && !disabled,
 
         // DISABLED
-        "cursor-not-allowed border-N3 bg-N2 text-N3": disabled,
+        "cursor-not-allowed border-N3 bg-N2 text-N3": !variant && disabled,
 
         // SIZE
-        "gap-1 px-3 py-2": size === "sm",
-        "gap-1 px-4 py-2.5": size === "md",
-        "gap-2 px-5 py-3": size === "lg",
-        "gap-2 px-6 py-3.5": size === "xl",
-        "gap-2 px-7 py-4": size === "2xl",
-        "gap-2 px-8 py-5": size === "3xl",
+        "h-fit w-fit gap-1 px-3 py-2": size === "sm",
+        "h-7 w-24 gap-1": size === "sm-status",
+        "h-fit w-fit gap-1 px-4 py-2.5": size === "md",
+        "h-10 w-28 gap-1": size === "md-status",
+        "h-fit w-fit gap-2 px-5 py-3": size === "lg",
+        "h-fit w-fit gap-2 px-6 py-3.5": size === "xl",
+        "h-fit w-fit gap-2 px-7 py-4": size === "2xl",
+        "h-fit w-fit gap-2 px-8 py-5": size === "3xl",
 
         // FONT SIZE
-        "text-sm": size === "sm",
+        "text-sm": size === "sm" || size === "md-status",
+        "text-xs": size === "sm-status",
         "text-base": size === "md",
         "text-lg": size === "lg",
         "text-xl": size === "xl",
