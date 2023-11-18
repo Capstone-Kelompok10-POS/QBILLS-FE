543af37d9788a9e9cde79401e4b32d36d312e612
rskrwnm <dwireski63@gmail.com>
2023-11-18 18:42:52 +0800

    update-Button|Component

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -12,6 +12,22 @@ export const Button = ({ type, variant, size, startIcon, label, endIcon, onClick
           !variant && !disabled,
         "border-P4 text-P4 hover:border-P5 hover:bg-P5 hover:text-N1 active:scale-95 active:border-P6 active:bg-P6 active:text-N1":
           variant === "outline" && !disabled,
+        "border-S4 bg-S4 text-N1 hover:border-S5 hover:bg-S5 active:scale-95 active:border-S6 active:bg-S6":
+          variant === "success" && !disabled,
+        "border-S4 text-S4 hover:border-S5 hover:bg-S5 hover:text-N1 active:scale-95 active:border-S6 active:bg-S6 active:text-N1":
+          variant === "outline-success" && !disabled,
+        "border-E4 bg-E4 text-N1 hover:border-E5 hover:bg-E5 active:scale-95 active:border-E6 active:bg-E6":
+          variant === "error" && !disabled,
+        "border-E4 text-E4 hover:border-E5 hover:bg-E5 hover:text-N1 active:scale-95 active:border-E6 active:bg-E6 active:text-N1":
+          variant === "outline-error" && !disabled,
+        "border-W4 bg-W4 text-N1 hover:border-W5 hover:bg-W5 active:scale-95 active:border-W6 active:bg-W6":
+          variant === "warning" && !disabled,
+        "border-W4 text-W4 hover:border-W5 hover:bg-W5 hover:text-N1 active:scale-95 active:border-W6 active:bg-W6 active:text-N1":
+          variant === "outline-warning" && !disabled,
+        "border-I4 bg-I4 text-N1 hover:border-I5 hover:bg-I5 active:scale-95 active:border-I6 active:bg-I6":
+          variant === "info" && !disabled,
+        "border-I4 text-I4 hover:border-I5 hover:bg-I5 hover:text-N1 active:scale-95 active:border-I6 active:bg-I6 active:text-N1":
+          variant === "outline-info" && !disabled,
 
         // DISABLED
         "cursor-not-allowed border-N3 bg-N2 text-N3": !variant && disabled,
