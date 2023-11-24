a9d2fec170eee80916f139aaca6de77e1acb26f1
dikaapradanaaa <dikapradana939@gmail.com>
2023-11-24 13:38:18 +0700

    update - Membership

--- src/modules/membership/main/index.jsx
+++ src/modules/membership/main/index.jsx
@@ -1,159 +1,73 @@
 "use client";
 import { useState } from "react";
-import { Button, Input, Table, Chip, Pagination } from "@/components";
+import { Button, Input, Table, Pagination } from "@/components";
 import SearchIcon from "@mui/icons-material/Search";
 export const Main = () => {
   const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const tableHead = [
-    "Code Transaction",
-    "Cashier",
-    "Customer Name",
-    "Payment Method",
-    "Date",
-    "Transaction",
-    "Status",
+    [],
+    "Name",
+    "Number Phone",
+    "Point",
+    [],
   ];
   const data = [
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Pending",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Success",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Canceled",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Pending",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Canceled",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Pending",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Success",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Canceled",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Pending",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
     {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Canceled",
+      name: "Dika Pradana",
+      numberphone: "083463514268",
+      point: "256 Point",
     },
-    {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Pending",
-    },
-    {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Success",
-    },
-    {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Canceled",
-    },
-    {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Pending",
-    },
-    {
-      ct: "MND1234",
-      cashier: "Victor Yoga",
-      cn: "Randy Donin",
-      payment: "QRIS BCA",
-      date: "14/11/2023",
-      transaction: "Rp 56.000",
-      status: "Canceled",
-    },
+    
   ];
   const filteredData = data.filter((row) => {
     const matchesSearch =
-      row.ct.toLowerCase().includes(search.toLowerCase()) ||
+      row.name.toLowerCase().includes(search.toLowerCase()) ||
       row.cn.toLowerCase().includes(search.toLowerCase());
     return matchesSearch;
   });
@@ -187,21 +101,15 @@ export const Main = () => {
         <Table tableHead={tableHead}>
           {currentData.map((row, index) => (
             <tr key={index} className={`${index % 2 === 0 ? "bg-N1" : "bg-N2.2"}`}>
-              <td className="px-4 py-2 text-center">{row.ct}</td>
-              <td className="px-4 py-2 text-center">{row.cashier}</td>
-              <td className="px-4 py-2 text-center">{row.cn}</td>
-              <td className="px-4 py-2 text-center">{row.payment}</td>
-              <td className="px-4 py-2 text-center">{row.date}</td>
-              <td className="px-4 py-2 text-center">{row.transaction}</td>
-              <td className="px-4 py-2">
-                <div className="flex items-center justify-center">
-                  <Chip
-                    status={row.status.toLocaleLowerCase()}
-                    size={"md-status"}
-                    label={row.status}
-                  />
-                </div>
-              </td>
+              {/* Checkbox */}
+              <td className="px-4 py-2 text-center"> Checkbox</td>
+
+              <td className="px-4 py-2 text-center">{row.name}</td>
+              <td className="px-4 py-2 text-center">{row.numberphone}</td>
+              <td className="px-4 py-2 text-center">{row.point}</td>
+              
+              {/* Action */}
+              <td className="px-4 py-2 text-center"> Action </td>
             </tr>
           ))}
         </Table>
