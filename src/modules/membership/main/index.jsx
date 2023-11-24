539620ebcacdf8dce70d5470436f81c13d89fad1
Ananda Lestari <anandalestari91@gmail.com>
2023-11-24 19:39:07 +0700

    update - Membership

--- src/modules/membership/main/index.jsx
+++ src/modules/membership/main/index.jsx
@@ -1,17 +1,11 @@
 "use client";
 import { useState } from "react";
-import { Button, Input, Table, Pagination } from "@/components";
+import { Button, Input, Table, Checkbox, Pagination } from "@/components";
 import SearchIcon from "@mui/icons-material/Search";
 export const Main = () => {
   const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const tableHead = [
-    [],
-    "Name",
-    "Number Phone",
-    "Point",
-    [],
-  ];
+  const tableHead = [[], "Name", "Number Phone", "Point", []];
   const data = [
     {
       name: "Dika Pradana",
@@ -63,7 +57,6 @@ export const Main = () => {
       numberphone: "083463514268",
       point: "256 Point",
     },
-    
   ];
   const filteredData = data.filter((row) => {
     const matchesSearch =
@@ -76,6 +69,15 @@ export const Main = () => {
   const indexOfFirstData = indexOfLastData - perPage;
   const currentData = filteredData.slice(indexOfFirstData, indexOfLastData);
   const totalPage = Math.ceil(filteredData.length / perPage);
+
+  const [selectedRow, setSelectedRow] = useState([]);
+  const handleCheckboxChange = (rowId) => {
+    setSelectedRow((prevSelectedRow) =>
+      prevSelectedRow.includes(rowId)
+        ? prevSelectedRow.filter((id) => id !== rowId)
+        : [...prevSelectedRow, rowId],
+    );
+  };
   return (
     <main className="space-y-5">
       <section className="flex w-full items-center gap-5">
@@ -101,13 +103,16 @@ export const Main = () => {
         <Table tableHead={tableHead}>
           {currentData.map((row, index) => (
             <tr key={index} className={`${index % 2 === 0 ? "bg-N1" : "bg-N2.2"}`}>
-              {/* Checkbox */}
-              <td className="px-4 py-2 text-center"> Checkbox</td>
+              <td className="px-4 py-2 text-center">
+                <div className="flex items-center justify-center">
+                  <Checkbox onChange={() => handleCheckboxChange(row.id)} />
+                </div>
+              </td>
 
               <td className="px-4 py-2 text-center">{row.name}</td>
               <td className="px-4 py-2 text-center">{row.numberphone}</td>
               <td className="px-4 py-2 text-center">{row.point}</td>
-              
+
               {/* Action */}
               <td className="px-4 py-2 text-center"> Action </td>
             </tr>
