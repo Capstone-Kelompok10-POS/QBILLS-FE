b01f1b3decce265897b719946ebc6d79b4bbd20e
dikaapradanaaa <dikapradana939@gmail.com>
2023-12-04 16:33:03 +0700

    update - Manage Product

--- src/modules/manageproduct/main/index.jsx
+++ src/modules/manageproduct/main/index.jsx
@@ -26,6 +26,7 @@ export const Main = () => {
   const [selectedRowCount, setSelectedRowCount] = useState(0);
   const [snackbar, setSnackbar] = useState(null);
   const [isAdd, setIsAdd] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
   const [category, setCategory] = useState("");
   const [name, setName] = useState("");
   const [ingredient, setIngredient] = useState("");
@@ -37,6 +38,14 @@ export const Main = () => {
 
   const categoryOptions = ["Coffee", "Non Coffee", "Snack", "Meal"];
   const sizeOptionsList = ["Small", "Large"];
+  const [editedData, setEditedData] = useState({
+    id: null,
+    name: "",
+    category: "",
+    size: "",
+    price: "",
+    stock: "",
+  });
 
   const [data, setData] = useState([
     {
@@ -62,7 +71,7 @@ export const Main = () => {
       code: "ABCD1236",
       name: "Alpa",
       category: "Coffee",
-      size: "Medium",
+      size: "Large",
       price: "Rp 25.000",
       stock: "200",
     },
@@ -98,7 +107,7 @@ export const Main = () => {
       code: "ABCD12310",
       name: "Alpa",
       category: "Coffee",
-      size: "Big",
+      size: "Large",
       price: "Rp 25.000",
       stock: "200",
     },
@@ -116,7 +125,7 @@ export const Main = () => {
       code: "ABCD12312",
       name: "Alpa",
       category: "Coffee",
-      size: "Big",
+      size: "Large",
       price: "Rp 25.000",
       stock: "200",
     },
@@ -305,6 +314,35 @@ export const Main = () => {
   const indexOfFirstData = indexOfLastData - perPage;
   const currentData = filteredData.slice(indexOfFirstData, indexOfLastData);
   const totalPage = Math.ceil(filteredData.length / perPage);
+
+  const handleEdit = (rowId) => {
+    const rowToEdit = data.find((row) => row.id === rowId);
+    setEditedData({
+      id: rowToEdit.id,
+      name: rowToEdit.name,
+      category: rowToEdit.category,
+      size: rowToEdit.size,
+      price: rowToEdit.price,
+      stock: rowToEdit.stock,
+    });
+    setIsEdit(true);
+  };
+
+  const handleEditSave = () => {
+    setIsEdit(false);
+    setSnackbar({
+      variant: "success",
+      size: "sm",
+      label: "Success",
+      desc: `Congratulations, you have successfully Edit Membership`,
+      onClickClose: handleCloseSnackbar,
+      onClickAction: {},
+    });
+
+    setTimeout(() => {
+      setSnackbar(null)
+    }, 5000);
+  };
 
   const handleDeleteIcon = (rowId) => {
     setSnackbar({
@@ -834,6 +872,177 @@ export const Main = () => {
                       size={"md-full"}
                       label={"Save"}
                       onClick={handleAddsave}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+
+      {/* POP UP EDIT PRODUCT */}
+      {isEdit && (
+        <section className="fixed -inset-5 z-50 flex items-center justify-center bg-black/50">
+          <div className=" w-3/5 rounded-xl bg-N1">
+            <div className="flex flex-col items-start gap-10 self-stretch p-10">
+              <h1 className="text-2xl font-semibold">Edit Product</h1>
+              <div className="flex items-start justify-between gap-6 self-stretch">
+                <div className="flex w-1/2 flex-col gap-5 self-stretch">
+                  <div className="flex flex-col gap-2">
+                    <h2 className="font-semibold">Category</h2>
+                    <Select
+                      size="md"
+                      label="Category"
+                      name="category"
+                      options={categoryOptions}
+                      value={editedData.category}
+                      onChange={(e) => setEditedData({...editedData, category: e.target.value})}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h2 className="font-semibold">Name</h2>
+                    <Input
+                      label={"Product"}
+                      type={"text"}
+                      size={"sm"}
+                      value={editedData.name}
+                      onChange={(e) => setEditedData({...editedData, name: e.target.value})}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h2 className="font-semibold">Ingredient</h2>
+                    <Input
+                      label={"Desc"}
+                      type={"text"}
+                      size={"md"}
+                      
+                    />
+                  </div>
+                  <div className="flex gap-5">
+                    <div className="flex w-full flex-col gap-2">
+                      <h2 className="font-semibold">Stock</h2>
+                      <Input
+                        label={"Available"}
+                        type={"text"}
+                        size={"sm"}
+                        value={editedData.stock}
+                        onChange={(e) => setEditedData({...editedData, stock: e.target.value})}
+                      />
+                    </div>
+                    <div className="flex w-full flex-col gap-2">
+                      <h2 className="font-semibold">Price</h2>
+                      <Input
+                        label={"Rp"}
+                        type={"text"}
+                        size={"sm"}
+                        value={editedData.price}
+                        onChange={(e) => setEditedData({...editedData, price: e.target.value})}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 self-stretch">
+                    <div className="flex items-center justify-between gap-2 self-stretch">
+                      <h2 className="font-semibold">Size</h2>
+                      <div className="flex items-start gap-2">
+                        <IconButton
+                          size={"sm"}
+                          variant={"outline"}
+                          icon={<AddIcon fontSize="small" />}
+                          onClick={handleAddSize}
+                        />
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <div className="flex w-full flex-col gap-2">
+                        <h3 className="text-N2">Detail</h3>
+                      </div>
+                      <div className="flex w-full flex-col gap-2">
+                        <h3 className="text-N2">Desc</h3>
+                      </div>
+                    </div>
+
+                    {sizeOptions.map((sizeOption, index) => (
+                      <div key={index} className="flex items-center gap-2">
+                        <div className="flex w-full flex-col gap-2">
+                          <Select
+                            size="md"
+                            label="Size"
+                            name={`size-${index}`}
+                            options={sizeOptionsList}
+                            value={editedData.size}
+                            onChange={(e) => setEditedData({...editedData, size: e.target.value})}
+                          />
+                        </div>
+                        <div className="flex w-full flex-col gap-2">
+                          <Input
+                            label={"Desc"}
+                            type={"text"}
+                            size={"sm"}
+                            value={sizeOption.price}
+                            onChange={(e) => handleDesc(index, e.target.value)}
+                          />
+                        </div>
+                        <IconButton
+                          size={"sm"}
+                          variant={"outline"}
+                          icon={<DeleteIcon fontSize="small" />}
+                          onClick={() => handleDeleteSize(index)}
+                        />
+                      </div>
+                    ))}
+                    
+                  </div>
+                </div>
+                <div className="flex w-1/2 flex-col gap-5 self-stretch">
+                  <div className="flex flex-col gap-2">
+                    <h2 className="font-semibold">Product Image</h2>
+                    <label htmlFor="file-input" className="cursor-pointer">
+                      <div className="h-80 w-full border border-N2">
+                        {!uploadedImage && !imagePreview && (
+                          <div className="flex h-full content-center items-center justify-center">
+                            <CloudUploadIcon className="text-8xl text-N2" />
+                          </div>
+                        )}
+                        {imagePreview && (
+                          <Image
+                            src={imagePreview}
+                            alt="Uploaded Product"
+                            className="h-full w-full object-cover"
+                            width={800}
+                            height={600}
+                          />
+                        )}
+                      </div>
+                    </label>
+                    <input
+                      type="file"
+                      id="file-input"
+                      accept="image/*"
+                      style={{ display: "none" }}
+                      onChange={(e) => handleFileChange(e)}
+                    />
+                  </div>
+                  <Button
+                    type={"button"}
+                    variant={"outline"}
+                    size={"md-full"}
+                    label={"Upload Image"}
+                    onClick={handleUploadButtonClick}
+                  />
+                  <div className="flex items-center gap-6 self-stretch">
+                    <Button
+                      type={"button"}
+                      variant={"outline"}
+                      size={"md-full"}
+                      label={"Cancel"}
+                      onClick={() => setIsEdit(false)}
+                    />
+                    <Button
+                      type={"button"}
+                      size={"md-full"}
+                      label={"Save"}
+                      onClick={handleEditSave}
                     />
                   </div>
                 </div>
