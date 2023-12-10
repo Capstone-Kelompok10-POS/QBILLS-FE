b3f819eb125ed27e82365e22bdd1a9a096555887
gdwmw <gededewowahyumw@gmail.com>
2023-12-11 01:05:47 +0700

    update - Dashboard

--- src/modules/dashboard/main/index.jsx
+++ src/modules/dashboard/main/index.jsx
@@ -21,8 +21,87 @@ import {
 import clsx from "clsx";
 import Image from "next/image";
 import { Line } from "react-chartjs-2";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 export const Main = () => {
+  const session = useSession();
+  const token = session.data?.user?.results.token;
+  const [dataTopMembership, setDataTopMembership] = useState();
+  const [dataRecentTransaction, setDataRecentTransaction] = useState();
+  const [dataBestSellingProduct, setDataBestSellingProduct] = useState();
+
+  const fetchTopMembership = async () => {
+    try {
+      const response = await fetch("https://www.ariefbook.my.id/api/v1/memberships/top", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Fetch request failed");
+      }
+
+      const data = await response.json();
+      setDataTopMembership(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const fetchRecentTransaction = async () => {
+    try {
+      const response = await fetch("https://www.ariefbook.my.id/api/v1/transactions/recent", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Fetch request failed");
+      }
+
+      const data = await response.json();
+      setDataRecentTransaction(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const fetchBestSellingProduct = async () => {
+    try {
+      const response = await fetch("https://www.ariefbook.my.id/api/v1/products/best", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Fetch request failed");
+      }
+
+      const data = await response.json();
+      setDataBestSellingProduct(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchTopMembership();
+      fetchRecentTransaction();
+      fetchBestSellingProduct();
+    }
+  }, [token]);
+
   ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
   const options = {
@@ -193,21 +272,6 @@ export const Main = () => {
     return price > 0 ? `+ ${formattedPrice}` : `- ${formattedPrice}`;
   };
 
-  const topMembership = [
-    { name: "Gede", point: 500 },
-    { name: "Dewo", point: 450 },
-    { name: "Wahyu", point: 300 },
-    { name: "Mustika", point: 225 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-    { name: "Wiwaha", point: 105 },
-  ];
-
   return (
     <main>
       <div className="flex gap-5">
@@ -249,15 +313,21 @@ export const Main = () => {
             <h2 className="text-2xl font-semibold">Best Selling Product</h2>
             <div className="rounded-lg border border-N2">
               <Table tableHead={tableHead}>
-                {tableData.map((row, index) => (
+                {dataBestSellingProduct?.results?.map((row, index) => (
                   <tr key={index} className={`${index % 2 === 0 ? "bg-N1" : "bg-N2.2"}`}>
                     <td className="px-4 py-2 text-center">
-                      <Image src={row.image} alt={"Photo Product"} width={30} className="mx-auto" />
+                      <Image
+                        src={row.productImage}
+                        alt={"Photo Product"}
+                        width={30}
+                        height={30}
+                        className="mx-auto"
+                      />
                     </td>
                     <td className="px-4 py-2 text-center">{row.productName}</td>
-                    <td className="px-4 py-2 text-center">{row.category}</td>
+                    <td className="px-4 py-2 text-center">{row.ProductTypeName}</td>
                     <td className="px-4 py-2 text-center">{row.price}</td>
-                    <td className="px-4 py-2 text-center">{row.quantity}</td>
+                    <td className="px-4 py-2 text-center">{row.totalQuantity}</td>
                     <td className="px-4 py-2 text-center">{row.amount}</td>
                   </tr>
                 ))}
@@ -268,28 +338,28 @@ export const Main = () => {
 
         <div className="flex h-auto w-[700px] flex-col gap-5">
           <section className="h-full space-y-5 rounded-lg border p-5">
-            <h2 className="text-2xl font-semibold">Transaction</h2>
-            <ul className="flex max-h-[500px] flex-col gap-5 overflow-y-scroll pr-5">
-              {transactionData.map((data, index) => (
+            <h2 className="text-2xl font-semibold">Recent Transaction</h2>
+            <ul className="flex max-h-[650px] min-h-[650px] flex-col gap-5 overflow-y-scroll pr-5">
+              {dataRecentTransaction?.results?.map((data, index) => (
                 <li className="flex items-center gap-5" key={index}>
                   <div className="flex h-14 w-14 items-center justify-center rounded-lg bg-N2.2">
-                    {data.price > 0 ? (
+                    {data.totalPayment > 0 ? (
                       <AccountBalanceWalletOutlinedIcon fontSize="large" className="text-S4" />
                     ) : (
                       <RemoveCircleOutlineOutlinedIcon fontSize="large" className="text-E4" />
                     )}
                   </div>
                   <div>
-                    <h3 className="text-lg font-semibold">{data.name}</h3>
-                    <p className="text-sm text-N3">{data.date}</p>
+                    <h3 className="text-lg font-semibold">{data.membership.name}</h3>
+                    <p className="text-sm text-N3">{data.createdAt}</p>
                   </div>
                   <p
                     className={clsx("ml-auto", {
-                      "text-S4": data.price > 0,
-                      "text-E4": data.price < 0,
+                      "text-S4": data.totalPayment > 0,
+                      "text-E4": data.totalPayment < 0,
                     })}
                   >
-                    {formatPrice(data.price)}
+                    {formatPrice(data.totalPayment)}
                   </p>
                 </li>
               ))}
@@ -307,8 +377,8 @@ export const Main = () => {
               <span className="mr-10">Point</span>
             </div>
 
-            <ul className="max-h-[290px] space-y-5 overflow-y-scroll font-semibold">
-              {topMembership.map((data, index) => (
+            <ul className="max-h-[160px] space-y-5 overflow-y-scroll font-semibold">
+              {dataTopMembership?.results?.map((data, index) => (
                 <li className="flex items-center justify-between" key={index}>
                   <div className="mx-5 flex items-center gap-5">
                     <span
