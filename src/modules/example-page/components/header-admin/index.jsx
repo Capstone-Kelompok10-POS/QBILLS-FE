0539a2d859311c3b3584d0e7b216d19051fa9230
gdwmw <gededewowahyumw@gmail.com>
2023-12-18 04:15:41 +0700

    update - Header Admin

--- src/modules/example-page/components/header-admin/index.jsx
+++ src/modules/example-page/components/header-admin/index.jsx
@@ -3,12 +3,7 @@ import { HeaderAdmin } from "@/components";
 export const ComponentHeaderAdmin = () => {
   return (
     <>
-      {/* 
-          name={""}
-          role={""}
-      */}
-
-      <HeaderAdmin name={"Example Name"} role={"Example Role"} />
+      <HeaderAdmin />
     </>
   );
 };
