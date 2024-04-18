8b9baaa332aaa4bc4e8cea02011f42b396866ce6
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-04-18 10:22:28 +0700

    update - Middleware

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -3,60 +3,54 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(request) {
-    if (
-      request.nextUrl.pathname.startsWith("/superadmin") &&
-      request.nextauth.token?.results.role !== "SuperAdmin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith("/dashboard") &&
-      request.nextauth.token?.results.role !== "SuperAdmin" &&
-      request.nextauth.token?.results.role !== "Admin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith("/manageaccount") &&
-      request.nextauth.token?.results.role !== "SuperAdmin" &&
-      request.nextauth.token?.results.role !== "Admin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith("/manageproduct") &&
-      request.nextauth.token?.results.role !== "SuperAdmin" &&
-      request.nextauth.token?.results.role !== "Admin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith("/membership") &&
-      request.nextauth.token?.results.role !== "SuperAdmin" &&
-      request.nextauth.token?.results.role !== "Admin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith("/transaction") &&
-      request.nextauth.token?.results.role !== "SuperAdmin" &&
-      request.nextauth.token?.results.role !== "Admin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith("/report") &&
-      request.nextauth.token?.results.role !== "SuperAdmin" &&
-      request.nextauth.token?.results.role !== "Admin"
-    ) {
-      return NextResponse.rewrite(new URL("/login", request.url));
-    }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/superadmin") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/dashboard") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin" &&
+    //   request.nextauth.token?.results.role !== "Admin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/manageaccount") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin" &&
+    //   request.nextauth.token?.results.role !== "Admin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/manageproduct") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin" &&
+    //   request.nextauth.token?.results.role !== "Admin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/membership") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin" &&
+    //   request.nextauth.token?.results.role !== "Admin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/transaction") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin" &&
+    //   request.nextauth.token?.results.role !== "Admin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/report") &&
+    //   request.nextauth.token?.results.role !== "SuperAdmin" &&
+    //   request.nextauth.token?.results.role !== "Admin"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/login", request.url));
+    // }
   },
   {
     callbacks: {
@@ -67,12 +61,12 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/superadmin",
-    "/dashboard",
-    "/manageaccount",
-    "/manageproduct",
-    "/membership",
-    "/transaction",
-    "/report",
+    // "/superadmin",
+    // "/dashboard",
+    // "/manageaccount",
+    // "/manageproduct",
+    // "/membership",
+    // "/transaction",
+    // "/report",
   ],
 };
